e173dd9f56bb9865feb2cea133d37ed6bc44a165
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-08-08 12:39:55 -0500

    created PUT endpoint for blog-posts

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -41,7 +41,33 @@ router.delete('/:id', (req, res) => {
   res.status(204).end();
 });
 
-
+// PUT (update) blog post
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with ID \`${req.params.id}\``);
+  const updatedBlog = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+  res.status(204).json(updatedBlog);
+})
 
 
 
