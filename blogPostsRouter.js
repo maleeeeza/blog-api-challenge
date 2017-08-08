5eeef7e91ba747841143cf31add5b14811e90c7a
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-08-08 12:38:34 -0500

    created POST endpoing for blog-posts

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -20,11 +20,22 @@ router.get('/', (req, res) => {
 
 
 // POST new blog posts
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
+});
 
 
-// DELETE blog post by id
 
 
-// PUT (update) blog post
 
 module.exports = router;
